a20382e31765fb0427903d55ed69f42d2a0ba2f7
Javier Catalán <74968085+javiercatalancas@users.noreply.github.com>
2021-02-22 10:34:08 +0100

    Initial commit

---
+++ app/src/main/java/com/ejercicio/applistadoelementos/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ejercicio.applistadoelementos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
